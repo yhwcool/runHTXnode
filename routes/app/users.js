f06edc151834fb75be5b840bfd8c660488de198c
Dcom_djl <499071615@qq.com>
2017-12-06 10:36:15 +0800

    1
    查看新增修改

--- routes/app/users.js
+++ routes/app/users.js
@@ -63,6 +63,11 @@ router.post('/regUser', function (req, res, next) {
     var currentmoney = params.currentmoney;//当前管理员的钱
     var currentstatus = params.currentstatus;//当前权限 
     var currentaccountid = params.currentaccountid;//当前权限 
+    var addtime = (function () {
+        var date = new Date();
+        return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDay() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    })();
+    params.addtime = addtime;
     var obj = {
         6: 0,
         5: 350,
@@ -101,13 +106,15 @@ router.post('/regUser', function (req, res, next) {
                                     account: params.uppersion
                                 }).fetch().then(function (uppersionone) { //查询上级，并且增加人数
                                     console.log('666666666');
-                                    console.log(uppersionone);
-                                    var obj = {
-                                        persionone: params.uppersion,
-                                        persiontwo: '',
-                                        persionthr: ''
-                                    }
+
                                     if (uppersionone) {
+
+                                        console.log(uppersionone);
+                                        var obj = {
+                                            persionone: params.uppersion,
+                                            persiontwo: '',
+                                            persionthr: ''
+                                        }
                                         obj.persiontwo = uppersionone.attributes.uppersion;
                                         console.log('persionone:' + obj.persiontwo);
                                         models.User.forge({
@@ -117,39 +124,28 @@ router.post('/regUser', function (req, res, next) {
                                                 obj.persionthr = persionthr.attributes.uppersion;
                                                 console.log('persiontwo:' + obj.persionthr)
                                             }
+                                            obj.account = params.account;
+                                            obj.addtime = addtime;
+                                            console.log(obj);
+                                            models.Persion.forge().save(obj).then(function (result) {
+                                                console.log('=========result========');
+                                                console.log(result);
+                                                if (result) {
+                                                    res.json({
+                                                        success: true,
+                                                        data: user,
+                                                        msg: '添加成功'
+                                                    })
+                                                }
+
+                                            })
                                         })
-                                        console.log(obj);
-                                        // models.Persion.forge().save({
-                                        //     account : params.account,
-                                        //     status : 1,
-                                        //     persionone : uppersion.attributes.account
-                                        // }).then(function(result){
-                                        //     console.log('result==================');
-                                        //     console.log(result);
-                                        // })
-                                        // var money = uppersion.attributes.money;
-                                        // var firstpersion = uppersion.attributes.firstpersion;
-                                        // console.log('money 111: ' + money);
-                                        // uppersion.save({
-                                        //     money: money + objfirst[1]
-                                        // }).then(function (uppersion) {
-                                        //     res.json({
-                                        //         success: true,
-                                        //         data: user,
-                                        //         msg: '上级更新成功'
-                                        //     })
-                                        // })
-                                    }else{
+                                    } else {
                                         res.json({
-                                        success: false,
-                                        msg: '上级用户不存在'
-                                    })
+                                            success: false,
+                                            msg: '上级用户不存在'
+                                        })
                                     }
-                                    // res.json({
-                                    //     success: true,
-                                    //     data: user,
-                                    //     msg: '添加成功'
-                                    // })
                                 })
                             })
                         }
@@ -178,106 +174,78 @@ router.post('/queryPersion', function (req, res, next) {
     var params = req.body;
     var status = params.persionstatus;
     var account = params.account;
-    var firstPersion;
-    var secpersion = [];
-    var thrpersion=[];
-    models.User.forge().fetchAll().then(function (user) {
-        console.log("length:" + user.models.length);
-        if (status == 0) { //查询一级代理
-            user.query('where', 'uppersion', '=', account).fetch().then(function (model) {
-                console.log("length:" + model.models.length);
-                if (model) {
-                    firstPersion = model.models; //一级代理
-                    res.json({
-                        success: true,
-                        data: model.models
-                    })
-                }
-            })
-        } else if (status == 1) { //查询二级代理
-            user.query('where', 'uppersion', '=', account).fetch().then(function (model) { //查询一级
-                if (model) {
-                    for (var i = 0; i < model.models.length; i++) {
-                        var account = model.models[i].attributes.account;
-                        console.log('account : ' + account);
-                        model.query('where', 'uppersion', '=', account).fetch().then(function (model) {
-                            for(var j=0;j<model.models.length;j++){
-                                secpersion.push(model.models[j].attributes);
-                            }
-                            if(secpersion.length >=25){ //判断时候完成任务
-
-                            }
-                            res.json({
-                                success: true,
-                                data: secpersion
-                            })
-                        })
-                    }
-                }
-            })
-        }else if(status == 2){ //三级代理
-             user.query('where', 'uppersion', '=', account).fetch().then(function (model) { //查询一级
-                if (model) {
-                    for (var i = 0; i < model.models.length; i++) {
-                        var account = model.models[i].attributes.account;
-                        console.log('account : ' + account);
-                        model.query('where', 'uppersion', '=', account).fetch().then(function (model) {
-                            for(var j=0;j<model.models.length;j++){
-                                secpersion.push(model.models[j].attributes);
-                            }
-                            if(secpersion.length >=25){ //判断时候完成任务
-                                     
-                            }
-                            res.json({
-                                success: true,
-                                data: secpersion
-                            })
-                        })
-                    }
-                }
+    var obj = {
+        "1" : 'persionone',
+        "2" : 'persiontwo',
+        "3" : 'persionthr'
+    }
+    models.Persion.query('where', obj[params.persionstatus], '=', account).fetchAll().then(function (model) {
+        console.log('==========query persion ==========');
+        console.log(model.models.length);
+        if (model.models) {
+            res.json({
+                success: true,
+                data: model.models
             })
         }
+    })
 
-        // if (params.status != 6) {
-        //     user.query('where', 'status', '<', params.status).fetch().then(function (model) {
-        //         if (model) {
-        //             res.json({
-        //                 success: true,
-        //                 data: model.models
-        //             })
-        //         }
-        //     })
-        // } else {
-        //     res.json({
-        //         success: true,
-        //         data: user.models
-        //     })
-        // }
-    });
-
-
-
-    // models.User.forge({
-    //     account: req.body.account
-    // }).fetch().then(function (user) {
-    //     if (user) {
-    //         if (user.attributes.password === req.body.password) {
+    // models.User.forge().fetchAll().then(function (user) {
+    // console.log("length:" + user.models.length);
+    // if (status == 0) { //查询一级代理
+    //     user.query('where', 'uppersion', '=', account).fetch().then(function (model) {
+    //         console.log("length:" + model.models.length);
+    //         if (model) {
+    //             firstPersion = model.models; //一级代理
     //             res.json({
     //                 success: true,
-    //                 data: user
-    //             })
-    //         } else {
-    //             res.json({
-    //                 success: false,
-    //                 msg: '密码错误'
+    //                 data: model.models
     //             })
     //         }
-    //     } else {
-    //         res.json({
-    //             success: false,
-    //             msg: '账户不存在，请联系管理员'
-    //         })
-    //     }
+    //     })
+    // } else if (status == 1) { //查询二级代理
+    //     user.query('where', 'uppersion', '=', account).fetch().then(function (model) { //查询一级
+    //         if (model) {
+    //             for (var i = 0; i < model.models.length; i++) {
+    //                 var account = model.models[i].attributes.account;
+    //                 console.log('account : ' + account);
+    //                 model.query('where', 'uppersion', '=', account).fetch().then(function (model) {
+    //                     for (var j = 0; j < model.models.length; j++) {
+    //                         secpersion.push(model.models[j].attributes);
+    //                     }
+    //                     if (secpersion.length >= 25) { //判断时候完成任务
+
+    //                     }
+    //                     res.json({
+    //                         success: true,
+    //                         data: secpersion
+    //                     })
+    //                 })
+    //             }
+    //         }
+    //     })
+    // } else if (status == 2) { //三级代理
+    //     user.query('where', 'uppersion', '=', account).fetch().then(function (model) { //查询一级
+    //         if (model) {
+    //             for (var i = 0; i < model.models.length; i++) {
+    //                 var account = model.models[i].attributes.account;
+    //                 console.log('account : ' + account);
+    //                 model.query('where', 'uppersion', '=', account).fetch().then(function (model) {
+    //                     for (var j = 0; j < model.models.length; j++) {
+    //                         secpersion.push(model.models[j].attributes);
+    //                     }
+    //                     if (secpersion.length >= 25) { //判断时候完成任务
+
+    //                     }
+    //                     res.json({
+    //                         success: true,
+    //                         data: secpersion
+    //                     })
+    //                 })
+    //             }
+    //         }
+    //     })
+    // }
     // });
 });
 
